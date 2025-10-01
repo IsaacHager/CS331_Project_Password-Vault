a957bbf84154f8f6d5a63b8fe36f29e20cb46160
Isaac Hager <isaachager05@gmail.com>
2025-09-30 18:59:47 -0600

    create PasswordDatabase.java and PasswordManager.java

---
+++ PasswordDatabase.java
@@ -0,0 +1,9 @@
+/**
+ * Given hashed passwords, store and retrieve them to a local file.
+ * 
+ * @version 1.0
+ * @author Isaac Hager
+ */
+public class PasswordDatabase {
+    // TODO implement class
+}
---
+++ PasswordManager.java
@@ -0,0 +1,9 @@
+/**
+ * Manages user passwords, including encryption, storage, and verification.
+ * 
+ * @version 1.0
+ * @author Isaac Hager
+ */
+public class PasswordManager {
+    // TODO implement class
+}
